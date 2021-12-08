0afa7ae47ca81c28e033837ca90fe393f9945226
유순조 <tnswh2023@phpschool.com>
2021-12-08 18:52:16 +0900

    redis subscribe

--- src/ioServer/IoServer.ts
+++ src/ioServer/IoServer.ts
@@ -4,27 +4,35 @@ import {redisConnect, topic} from "../application/application";
 import redis from "redis";
 import Redis from "ioredis"
 
-const redis2 = new Redis()
-
 export const ioServer = () => {
     const io = new socket.Server(app, {});
-    const pub = redis.createClient(redisConnect);
-    const sub = redis.createClient(redisConnect);
+    const sub = new Redis(6379, "3.38.124.11");
+    const pub = new Redis(6379, "3.38.124.11");
+    console.log(topic)
+    sub.subscribe(topic, (err, count) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log("sub")
+        }
+    })
 
-    sub.on('subscribe', function (channel, count) {
-        console.log("subscribe")
-        console.log(channel, count)
-    });
+    sub.on("message", (channel, message) => {
+        console.log("sdf");
+        const object = JSON.parse(message);
+        console.log(message)
+        io.to(object.notificationMemberId).emit(object.notificationTemplate, object.boardId, object.commentMemberNickname);
+    })
 
-    sub.on('message', function (channel, message) {
-        console.log(channel, message)
-    });
-
+    // 프론트에서 emit 하면 그에 맞는 이벤트 발생시켜줌
     io.on('connect', (ioSocket) => {
         console.log(ioSocket.id);
         console.log("hello");
-        let broadcastId = ioSocket.handshake.query['broadcastId'];
-        console.log(broadcastId)
+        let memberId = ioSocket.handshake.query['memberId'];
+        console.log(memberId)
+        // @ts-ignore
+        ioSocket.join(memberId)
 
         ioSocket.on("NOTIFICATION", (response) => {
             io.to(ioSocket.id).emit('NOTIFICATION')
