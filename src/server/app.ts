de201a544bf6587ba56f7d58c1ba4ae5ace58592
유순조 <tnswh2023@phpschool.com>
2021-12-09 22:05:13 +0900

    cors

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,7 +2,13 @@ import {createServer} from "http";
 
 class Server {
     createServer() {
-        return createServer();
+        return createServer((req, res) => {
+            const headers = {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+                'Access-Control-Max-Age': 2592000, // 30 days
+            };
+        });
     }
 }
 const server: Server = new Server();
